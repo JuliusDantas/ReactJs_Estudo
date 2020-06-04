8ddc478e3ac927e2f54574bd0b282715aef645c5
Julius Cesar Machado Dantas <juliusmdantas@hotmail.com>
2020-06-04 18:12:41 -0300

    criado o arquivo {

--- lesson-01/configuration/src/index.js
+++ lesson-01/configuration/src/index.js
@@ -2,11 +2,25 @@ import react from "react"
 import reactDOM from "react-dom"
 import "./style.CSS"
 
+function sum(a, b) {
+    return a + b
+}
+
+function primeiroJSX() {
+    return (
+        <div className="title">
+            Julius Dantas - ReactJS
+        <h5> soma: {sum(20, 30)} </h5>
+        </div>
+    )
+    
+}
+
 const App = () => {
     
     return (
         <div className="APP">
-            Hello World
+            {primeiroJSX()}
         </div>
     )
 }
